5a5546f3385eb65499b8bca7367a05e4397a5327
lebedkinaer <80522784+lebedkinaer@users.noreply.github.com>
2021-03-18 13:37:42 +0300

    Вычисление расстояния между двумя точками в 2д плоскости

--- sandbox/src/main/java/ru/qa/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/qa/sandbox/MyFirstProg.java
@@ -2,9 +2,14 @@ package ru.qa.sandbox;
 
 public class MyFirstProg {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
 
+    Point p1 = new Point(4, 6);
+    Point p2 = new Point(10, 14);
+
+    System.out.println("Координаты первой точки по стороне x = " + p1.x + " по стороне y = " + p1.y);
+    System.out.println("Координаты второй точки по стороне x = " + p2.x + " по стороне y = " + p2.y);
+    System.out.println("Расстояние между точками = " + p1.distance(p2));
+  }
 }
