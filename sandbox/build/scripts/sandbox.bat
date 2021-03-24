ccad993aaceb258e11437d7ff37222d75034c13c
lebedkinaer <80522784+lebedkinaer@users.noreply.github.com>
2021-03-24 17:54:54 +0300

    Добавлен фреймворк TestNG

--- sandbox/build/scripts/sandbox.bat
+++ sandbox/build/scripts/sandbox.bat
@@ -67,7 +67,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\sandbox.jar
+set CLASSPATH=%APP_HOME%\lib\sandbox.jar;%APP_HOME%\lib\testng-7.4.0.jar;%APP_HOME%\lib\jcommander-1.78.jar;%APP_HOME%\lib\jquery-3.5.1.jar
 
 
 @rem Execute sandbox
---
+++ sandbox/src/test/java/ru/qa/sandbox/PointTests.java
@@ -0,0 +1,4 @@
+package ru.qa.sandbox;
+
+public class PointTests {
+}
