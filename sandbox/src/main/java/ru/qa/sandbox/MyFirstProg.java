e8ad93cf6430053e3a3638f3ec0af91c7bd3f387
lebedkinaer <80522784+lebedkinaer@users.noreply.github.com>
2021-04-05 16:57:39 +0300

    Создан тест для класса Point

--- sandbox/src/main/java/ru/qa/sandbox/MyFirstProg.java
+++ sandbox/src/main/java/ru/qa/sandbox/MyFirstProg.java
@@ -5,8 +5,8 @@ public class MyFirstProg {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
 
-    Point p1 = new Point(4, 6);
-    Point p2 = new Point(10, 14);
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(2, 16);
 
     System.out.println("Координаты первой точки по стороне x = " + p1.x + " по стороне y = " + p1.y);
     System.out.println("Координаты второй точки по стороне x = " + p2.x + " по стороне y = " + p2.y);
--- sandbox/src/test/java/ru/qa/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/qa/sandbox/PointTests.java
@@ -1,4 +1,21 @@
 package ru.qa.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class PointTests {
+
+  @Test
+  public void testDistance(){
+    Point p1 = new Point(4, 6);
+    Point p2 = new Point(10, 14);
+    Assert.assertEquals(p1.distance(p2), 10.0);
+  }
+
+  @Test
+  public void testDistanceBackward(){
+    Point p1 = new Point(10, 14);
+    Point p2 = new Point(4, 6);
+    Assert.assertEquals(p2.distance(p1), 10.0);
+  }
 }
